b93fc02774dea2a3739aea55e563dd212509d806
Murendeni Mukwevho <murendenim@girlcode.co.za>
2023-10-11 21:47:15 +0200

    Added Brands Sections

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import HeroSection from "./components/HeroSection"
 import './App.scss'
 import ServicesSection from "./components/ServicesSection";
 import CaseStudiesSection from "./components/CaseStudiesSection";
+import BrandsSection from "./components/BrandsSection";
 
 function App() {
 	return (
@@ -11,6 +12,7 @@ function App() {
 			<HeroSection />
 			<ServicesSection />
 			<CaseStudiesSection />
+			<BrandsSection />
 		</>
 	)
 }
