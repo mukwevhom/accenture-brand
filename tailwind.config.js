dc7ce7b742e0a266fc741f11bc2d3a06b341a396
Murendeni Mukwevho <murendenim@girlcode.co.za>
2023-10-11 19:34:16 +0200

    Added Navbar & Hero Sections

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,5 @@
+const defaultTheme = require('tailwindcss/defaultTheme')
+
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
@@ -5,7 +7,24 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    container: {
+      center: true,
+      padding: '1.5rem'
+    },
+    extend: {
+      fontFamily: {
+        inter: [
+          'Inter',
+          ...defaultTheme.fontFamily.sans
+        ]
+      },
+      colors: {
+        'accenture': {
+          400: '#D100C9',
+          900: '#70259B'
+        }
+      }
+    },
   },
   plugins: [],
 }
