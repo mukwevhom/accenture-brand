a7a6c5c0d603307ade09c7bb37e55cf14257a2c8
Murendeni Mukwevho <murendenim@girlcode.co.za>
2023-10-12 12:08:58 +0200

    Updated Readme and the slider

--- README.md
+++ README.md
@@ -1,27 +1,26 @@
-# React + TypeScript + Vite
+# Front-end React Developer Challenge
 
-This template provides a minimal setup to get React working in Vite with HMR and some ESLint rules.
+This project is for the Front-end React Developer Challenge for the Accenture frontend role
 
-Currently, two official plugins are available:
+This project is based on this [Figma design](https://www.figma.com/file/LeLJIweoqHTFBoeOIbQuru/Bulma-CSS-UI-Kit---Front-end-Test?type=design&node-id=309%3A12685&mode=dev)
 
-- [@vitejs/plugin-react](https://github.com/vitejs/vite-plugin-react/blob/main/packages/plugin-react/README.md) uses [Babel](https://babeljs.io/) for Fast Refresh
-- [@vitejs/plugin-react-swc](https://github.com/vitejs/vite-plugin-react-swc) uses [SWC](https://swc.rs/) for Fast Refresh
+You can find the case studies API url [here](https://zm6zxgq6hyhe3smi5krzsrk2fu0iidhh.lambda-url.us-east-1.on.aws), use this API on the .env
 
-## Expanding the ESLint configuration
+## Getting started
 
-If you are developing a production application, we recommend updating the configuration to enable type aware lint rules:
+**Prerequisites**
 
-- Configure the top-level `parserOptions` property like this:
+- [Node.js](https://nodejs.org/en/download/)
+- [NPM](https://docs.npmjs.com/cli/v10/commands/npm)
 
-```js
-   parserOptions: {
-    ecmaVersion: 'latest',
-    sourceType: 'module',
-    project: ['./tsconfig.json', './tsconfig.node.json'],
-    tsconfigRootDir: __dirname,
-   },
-```
+With the above tools available, you are ready:
 
-- Replace `plugin:@typescript-eslint/recommended` to `plugin:@typescript-eslint/recommended-type-checked` or `plugin:@typescript-eslint/strict-type-checked`
-- Optionally add `plugin:@typescript-eslint/stylistic-type-checked`
-- Install [eslint-plugin-react](https://github.com/jsx-eslint/eslint-plugin-react) and add `plugin:react/recommended` & `plugin:react/jsx-runtime` to the `extends` list
+1. Clone the repository.
+
+2. Install dependencies
+   ```npm install```
+
+3. Create `.env` file based on the `.env.example` file, and update the `VITE_CASE_STUDIES_API` variable with the link mentioned above.
+
+4. Run the dev server
+   ```npm run dev```
--- src/components/CaseStudiesSection.tsx
+++ src/components/CaseStudiesSection.tsx
@@ -1,8 +1,7 @@
-import { useEffect, useRef, useState } from "react"
-import BlazeSlider from 'blaze-slider'
-import type { BlazeConfig } from "blaze-slider";
+import { useEffect, useState } from "react"
+import { useKeenSlider, KeenSliderPlugin } from "keen-slider/react"
+import "keen-slider/keen-slider.min.css"
 import Section from "../layout/Section"
-import "blaze-slider/dist/blaze.css";
 
 interface CaseStudy {
     imageUrl: string;
@@ -10,35 +9,110 @@ interface CaseStudy {
     title: string;
 }
 
-function useBlazeSlider(config: BlazeConfig) {
-    const sliderRef = useRef<BlazeSlider>()
-    const elRef = useRef<HTMLDivElement | null>(null)
-  
-    useEffect(() => {
-        if (!sliderRef.current) {
-            sliderRef.current = new BlazeSlider(elRef.current!, config)
-        }
-
-        // if (sliderRef.current) {
-        //     sliderRef.current.destroy()
-        //     sliderRef.current = new BlazeSlider(elRef.current!, config)
-        // }
-    }, [])
-  
-    return elRef
+interface ArrowProps {
+    disabled: boolean;
+    left?: boolean;
+    onClick: (e: any) => void
 }
+
+const MutationPlugin: KeenSliderPlugin = (slider) => {
+    const observer = new MutationObserver(function (mutations) {
+        mutations.forEach(function () {
+            slider.update()
+        })
+    })
+
+    const config = { childList: true }
+  
+    slider.on("created", () => {
+        observer.observe(slider.container, config)
+    })
+    slider.on("destroyed", () => {
+        observer.disconnect()
+    })
+}
+
+const AutoSwitchPlugin: KeenSliderPlugin = (slider) => {
+    let timeout: ReturnType<typeof setTimeout>
+    let mouseOver = false
+
+    function clearNextTimeout() {
+        clearTimeout(timeout)
+    }
+
+    function nextTimeout() {
+        clearTimeout(timeout)
+        if (mouseOver) return
+        timeout = setTimeout(() => {
+            slider.next()
+        }, 2000)
+    }
+
+    slider.on("created", () => {
+        slider.container.addEventListener("mouseover", () => {
+            mouseOver = true
+            clearNextTimeout()
+        })
+        slider.container.addEventListener("mouseout", () => {
+            mouseOver = false
+            nextTimeout()
+        })
+        nextTimeout()
+    })
+
+    slider.on("dragStarted", clearNextTimeout)
+    slider.on("animationEnded", nextTimeout)
+    slider.on("updated", nextTimeout)
+}
+
+function Arrow(props: ArrowProps) {
+    const disabeld = props.disabled ? " arrow--disabled" : ""
+
+    return (
+        <svg
+            onClick={props.onClick}
+            className={`arrow ${props.left ? "arrow--left" : "arrow--right"} ${disabeld}`}
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 24 24" >
+            {props.left && (
+                <path d="M16.67 0l2.83 2.829-9.339 9.175 9.339 9.167-2.83 2.829-12.17-11.996z" />
+            )}
+            {!props.left && (
+                <path d="M5 3l3.057-3 11.943 12-11.943 12-3.057-3 9-9z" />
+            )}
+        </svg>
+    )
+  }
 
 function Content () {
     const [caseStudies, setCaseStudies] = useState<CaseStudy[] | []>([])
-    const elRef = useBlazeSlider({ 
-        all: {
-            enableAutoplay: true,
-            slidesToShow: 3,
+    const [currentSlide, setCurrentSlide] = useState<number>(0)
+    const [loaded, setLoaded] = useState<boolean>(false)
+    const [sliderRef, instanceRef] = useKeenSlider<HTMLDivElement>(
+        {
+            initial: 0,
+            loop: true,
+            mode: "free-snap",
+            slides: {
+                perView: 3,
+                spacing: 15,
+            },
+            breakpoints: {
+                '(max-width: 575px)': {
+                    slides: {
+                        perView: 1,
+                    }
+                },
+            },
+            slideChanged(slider) {
+                setCurrentSlide(slider.track.details.rel)
+            },
+            created() {
+                setLoaded(true)
+            },
         },
-        "(max-width: 575px)": {
-            slidesToShow: 1,
-        }
-    })
+        [AutoSwitchPlugin, MutationPlugin]
+    )
 
     const fetchCaseStudies = () => {
         return fetch(import.meta.env.VITE_CASE_STUDIES_API)
@@ -51,21 +125,29 @@ function Content () {
     }, [])
 
     return (
-        <div className="blaze-slider" ref={elRef}>
-            <div className="blaze-container">
-                <div className="blaze-track-container">
-                    <div className="case-studies blaze-track">
-                        { caseStudies.map((caseStudy, idx) => (
-                            <div className="case-study-item" key={`case-study-${idx}`} style={{backgroundImage: `url(${caseStudy?.imageUrl})`}}>
-                                <a href="#" className="case-study-content">
-                                    <h3>{caseStudy?.title}</h3>
-                                    <p>{caseStudy?.description}</p>
-                                </a>
-                            </div>
-                        ))}
+        <div className="navigation-wrapper">
+            <div className="case-studies keen-slider" ref={sliderRef}>
+                { caseStudies.map((caseStudy, idx) => (
+                    <div className="keen-slider__slide case-study-item" key={`case-study-${idx}`} style={{backgroundImage: `url(${caseStudy?.imageUrl})`}}>
+                        <a href="#" className="case-study-content">
+                            <h3>{caseStudy?.title}</h3>
+                            <p>{caseStudy?.description}</p>
+                        </a>
                     </div>
-                </div>
+                ))}
             </div>
+            {loaded && instanceRef.current && (
+                <>
+                    <Arrow left
+                        onClick={(e: any) => e.stopPropagation() || instanceRef.current?.prev() }
+                        disabled={currentSlide === 0} />
+
+                    <Arrow
+                        onClick={(e: any) => e.stopPropagation() || instanceRef.current?.next() }
+                        disabled={ currentSlide === instanceRef.current.track.details?.slides.length - 1 }
+                    />
+                </>
+            )}
         </div>
     )
 }
